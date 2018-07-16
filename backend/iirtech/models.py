e69b15595045fd64add6aa6e4ff089d0768b8b43
kyungjejo <kyungjejo@gmail.com>
2018-07-16 14:58:03 +0900

    initialize handler

--- backend/iirtech/models.py
+++ backend/iirtech/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class QuestionType(models.Model):
+    questionType = models.IntegerField()
+    questionID = models.IntegerField()
+    dialogueIndex = models.IntegerField()
--- backend/iirtech/views.py
+++ backend/iirtech/views.py
@@ -1,11 +1,74 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from iirtech.models import QuestionType
 import json
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import os, uuid
+
+STATIC_PATH = './backend/static/'
+
+lines = open(static('scenario.txt')).readlines()
+
+class Bot():
+    def __init__(self):
+        self.index = 0
+        self.lines = lines
+        self.id = uuid.uuid4()
+    
+    def next_line(self):
+        line = self.lines[self.index]
+        self.index += 1
+        return line
 
 # Create your views here.
 def fetchMessage(request):
-    msg = request.GET['text']
-    if msg:
-        print(msg)
-        js = {'success': 'success', 'msg': msg}
-        return HttpResponse(json.dumps(js), content_type="application/json")
+    """
+        input: POST
+
+            (str) text: message
+            (int) type: 0 - initialize, 1 - question, 2 - normal message
+            (int) index: -1 - initialize and question type, 0 - normal message
+            (str) userid: id
+
+        return: JSON
+
+            (str) text: message 
+            (int) type: 0 - initialize, 1 - question, 2 - normal message
+            (int) success: 0 - fail, 1 - success
+            (str) userid: id
+
+    """
+    _type = request.GET['type']
+    if _type == 0:
+        bot = Bot()
+        msg = bot.lines[bot.index]
+        userid = bot.id
+        js = {
+            "text": msg,
+            "type": 0,
+            "success": 1,
+            "userid": userid
+        }
+    elif _type == 1:
+        js = {}
+    else:
+        msg = request.GET['text']
+        js = {}
+    
+    return HttpResponse(json.dumps(js), content_type="application/json")
+
+def returnQuestion(request):
+    """
+
+       input: JSON
+
+
+
+       return: JSON
+
+
+
+    """
+
+    js = {'success': 'success'}
+    return HttpResponse(json.dumps(js), content_type="application/json")
