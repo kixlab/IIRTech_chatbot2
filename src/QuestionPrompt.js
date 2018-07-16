c6d8723a5a93e4545309d42447aab70e34ccec33
Changyoon <cyoon47@hotmail.com>
2018-07-16 14:59:44 +0900

    merge

---
+++ src/QuestionPrompt.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class QuestionPrompt extends React.Component {
+  render(){
+    return(
+      <div>
+        <div className="questionText">위 문장에서 특정 유형의 질문이 있으신가요?</div>
+        <div style={{textAlign:'center'}}>
+            <ol className="questionList">
+            <li className="questionBtn">
+              <input className="buttonText" type="button" value="1. 어휘"/>
+            </li>
+            <li className="questionBtn">
+              <input className="buttonText" type="button" value="2. 문법"/>
+            </li>
+            <li className="questionBtn">
+              <input className="buttonText" type="button" value="3. 발음"/>
+            </li>
+            <li className="questionBtn">
+              <input className="buttonText" type="button" value="4. 기타"/>
+            </li>
+            <li className="questionBtn">
+              <input className="buttonText" type="button" value="5. 없음"/>
+            </li>
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default QuestionPrompt;
