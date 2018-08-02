89ecdd477b61616b12450f359f10ffabc564494d
Changyoon <cyoon47@hotmail.com>
2018-08-02 15:45:12 +0900

    MessageBox scrolling added

--- src/MessageBox.jsx
+++ src/MessageBox.jsx
@@ -3,6 +3,22 @@ import './MessageBox.css';
 import Message from './Message'
 class MessageBox extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+  }
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({behavior: "smooth"});
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
     const messageList = this.props.messageLog.map((message, index) => {
       return (
@@ -12,6 +28,9 @@ class MessageBox extends React.Component {
     return (
       <div className='messagebox'>
         {messageList}
+        <div
+          ref={(el) => {this.messagesEnd = el; }}>
+        </div>
       </div>
     );
   }
