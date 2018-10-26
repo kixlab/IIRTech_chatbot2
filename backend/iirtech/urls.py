5625f4db241bbf9236690bdc4de531c041e477b9
kyungjejo <kyungjejo@gmail.com>
2018-10-26 19:19:54 +0900

    minor error in urls.py

--- backend/iirtech/urls.py
+++ backend/iirtech/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url('fetchMessage', views.fetchMessage, name="fetchMessage"),
-    url('fetchActivity', views.fetchActivity, name='fetchActivity')
+    url('fetchActivity', views.fetchActivity, name='fetchActivity'),
     url('chooseTense', views.chooseTense, name="chooseTense")
 ]
