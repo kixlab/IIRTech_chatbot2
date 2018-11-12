773cddf752472542e81b341ee4e087eb0a5b11db
Changyoon <cyoon47@hotmail.com>
2018-11-12 18:45:25 +0900

    logger

--- backend/iirtech/views.py
+++ backend/iirtech/views.py
@@ -29,7 +29,7 @@ class Bot():
         self.id = str(uuid.uuid4())
         self.tense = None
         self.replace_pairs = []
-        self.log_file = open(static("log/" + self.id + ".log"), "a")
+        self.log_file = open(static("log/" + self.id + ".txt"), "a")
         self.log_file.write("Log start at " + str(datetime.datetime.now()) + "\n")
 
     def next_line(self):
